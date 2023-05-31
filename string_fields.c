a02c3ebb94851254f82b5001686a645ae61e354a
Oumfayselfariss <fayselfariss22@gmail.com>
2023-05-30 18:57:45 -0700

    correction code string_fields

--- string_fields.c
+++ string_fields.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
 /**
+* get_width - gets the width from the format string
+* @p: the format string
+* @params: the parameters struct
+* @ap: the argument pointer
+*
+* Return: new pointer
+*/
+char *get_width(char *p, params_t *params, va_list ap)
+{
+	int w = 0;
+
+	if (*p == '*')
+	{
+		w = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
+		while (isdigit((unsigned char)*p))
+		{
+			w = w * 10 + (*p - '0');
+			p++;
+		}
+	}
+
+	params->width = w;
+	return (p);
+}
+
+/**
 * get_precision - gets the precision from the format string
 * @p: the format string
 * @params: the parameters struct
@@ -10,27 +40,28 @@
 */
 char *get_precision(char *p, params_t *params, va_list ap)
 {
-        int d = 0;
+	int d = 0;
 
-        if (*p != '.')
-                return (p);
+	if (*p != '.')
+		return (p);
 
-        p++;
+	p++;
 
-        if (*p == '*')
-        {
-                d = va_arg(ap, int);
-                p++;
-        }
-        else
-        {
-                while (isdigit(*p))
-                {
-                        d = d * 10 + (*p++ - '0');
-                }
-        }
+	if (*p == '*')
+	{
+		d = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
+		while (isdigit((unsigned char)*p))
+		{
+			d = d * 10 + (*p - '0');
+			p++;
+		}
+	}
 
-        params->precision = d;
-        return (p);
+	params->precision = d;
+	return (p);
 }
 
