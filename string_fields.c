29e844f92fd3487de5c9ec72da26599390672290
Oumfayselfariss <fayselfariss22@gmail.com>
2023-05-30 18:32:29 -0700

    error code

--- string_fields.c
+++ string_fields.c
@@ -1,34 +1,36 @@
 #include "main.h"
 
 /**
- * get_precision - gets the precision from the format string
- * @p: the format string
- * @params: the parameters struct
- * @ap: the argument pointer
- *
- * Return: new pointer
- */
+* get_precision - gets the precision from the format string
+* @p: the format string
+* @params: the parameters struct
+* @ap: the argument pointer
+*
+* Return: new pointer
+*/
 char *get_precision(char *p, params_t *params, va_list ap)
 {
-	int d = 0;
+        int d = 0;
 
-	if (*p != '.')
-		return p;
+        if (*p != '.')
+                return (p);
 
-	p++;
+        p++;
 
-	if (*p == '*')
-	{
-		d = va_arg(ap, int);
-		p++;
-	}
-	else
-	{
-		while (isdigit(*p))
-			d = d * 10 + (*p++ - '0');
-	}
+        if (*p == '*')
+        {
+                d = va_arg(ap, int);
+                p++;
+        }
+        else
+        {
+                while (isdigit(*p))
+                {
+                        d = d * 10 + (*p++ - '0');
+                }
+        }
 
-	params->precision = d;
-	return p;
+        params->precision = d;
+        return (p);
 }
 
